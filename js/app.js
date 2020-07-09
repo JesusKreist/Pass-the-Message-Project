a475ad29b905e6d101a5197521301ba50f4a959a
JesusKreist <59575776+JesusKreist@users.noreply.github.com>
2020-07-09 19:42:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 let submitButton = document.getElementById("submitBtn");
 
 const handleForm = (event) => {
+    event.preventDefault()
     let message = document.getElementById("message").value;
     if (message) {
         document.querySelector(".message-content").innerHTML = message;
-        document.getElementById("message").value = "";
-        
-    } else {
-        event.preventDefault()
+        document.getElementById("message").value = "";        
+    } else {        
         document.querySelector(".feedback").classList.add("show");  
         setTimeout(() => {
             document.querySelector(".feedback").classList.remove("show");
