72ab52601b82ba019b49ff1dbb28586736393801
JesusKreist <59575776+JesusKreist@users.noreply.github.com>
2020-07-09 17:30:27 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,19 @@
+let submitButton = document.getElementById("submitBtn");
 
-(function() {
-//Select the input element
-const form = document.querySelector('#message-form')
-//Set up Submit Button
-form.addEventListener('submit', function(e){
-    // prevent the form's default submission action
-    e.preventDefault()
-    //Get user's input from from
-    const message = document.querySelector('#message')
-    const feedback = document.querySelector('.feedback')
-    const messageContent = document.querySelector('.message-content')
-
-    if (message.value === ''){
-        feedback.classList.add('show')
-        setTimeout(function(){
-        feedback.classList.remove('show')
-        }, 2000)
+const handleForm = (event) => {
+    let message = document.getElementById("message").value;
+    if (message) {
+        document.querySelector(".message-content").innerHTML = message;
+        document.getElementById("message").value = "";
+        
     } else {
-        //Change message content and clear the message input
-        messageContent.textContent = message.value
-        message.value = ''
-    }
-})
-})()
+        event.preventDefault()
+        document.querySelector(".feedback").classList.add("show");  
+        setTimeout(() => {
+            document.querySelector(".feedback").classList.remove("show");
+        }, 2000)
+        
+    };
+};
 
-
-
+submitButton.addEventListener("click", handleForm);
